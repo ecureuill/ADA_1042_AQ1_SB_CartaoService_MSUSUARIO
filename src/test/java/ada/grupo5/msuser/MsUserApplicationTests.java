a6e1988dc05a0016600a366a074b8050ab5d5e02
Camilla <logikasciuro@gmail.com>
2023-09-06 12:50:40 +0000

    🎉 Begin project

---
+++ src/test/java/ada/grupo5/msuser/MsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package ada.grupo5.msuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
